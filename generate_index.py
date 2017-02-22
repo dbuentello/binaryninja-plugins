7771ceddaa7a0471151e81328b3964e4c51f0f7b
Jordan Wiens <jordan@psifertex.com>
2017-02-21 23:36:06 -0500

    cleanup and include path

--- generate_index.py
+++ generate_index.py
@@ -14,28 +14,27 @@ args = parser.parse_args()
 os.path.realpath(__file__)
 
 
-basedir=os.path.join(os.path.dirname(os.path.realpath(__file__)), 'plugins')
+basedir = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'plugins')
 outputfile = os.path.join(basedir, 'README.md')
 
 if not args.force and os.path.isfile(outputfile):
 	print("Cowardly refusing to overwrite an existing README. Remove or re-run with -f.")
 	sys.exit(0)
 
-#channels = glob.glob(os.path.join(basedir,"*"))
 channels = os.walk(basedir).next()[1]
 
 template = '# Binary Ninja Plugins\n\n'
 
 config = configparser.ConfigParser()
 config.readfp(open(os.path.join(basedir, '..', '.gitmodules')))
-submodules={}
+submodules = {}
 
 for section in config.items()[1:]:
 	sectionname = section[0].split('"')[1]
 	submodules[sectionname] = config.get(section[0], 'url')
 
-for channel in ['official', 'community']: #because otherwise it's alphabetical
-	index = os.path.join(basedir,channel+".json")
+for channel in ['official', 'community']:  # Because otherwise it's alphabetical
+	index = os.path.join(basedir, channel + ".json")
 	if not args.force and os.path.isfile(index):
 		print("Cowardly refusing to overwrite an existing index. Remove or re-run with -f.")
 		sys.exit(0)
@@ -51,7 +50,7 @@ for channel in ['official', 'community']: #because otherwise it's alphabetical
 |------------|--------|---------|-------------|
 '''
 
-	for plugin in os.walk(os.path.join(basedir,channel)).next()[1]:
+	for plugin in os.walk(os.path.join(basedir, channel)).next()[1]:
 		try:
 			url = submodules[os.path.join('plugins', channel, plugin)]
 			if url.endswith('.git'):
@@ -60,8 +59,9 @@ for channel in ['official', 'community']: #because otherwise it's alphabetical
 		except:
 			url = 'https://github.com/Vector35/binaryninja-plugins/tree/master/plugins/{channel}/{plugin}'.format(channel = channel, plugin = plugin)
 			authorlink = 'https://github.com/Vector35/'
-		data = json.load(open(os.path.join(basedir,channel,plugin,"plugin.json")), object_pairs_hook=OrderedDict)['plugin']
+		data = json.load(open(os.path.join(basedir, channel, plugin, "plugin.json")), object_pairs_hook=OrderedDict)['plugin']
 		data['url'] = url
+		data['path'] = plugin
 		plugins.append(data)
 		template += '|[{name}]({url})|[{author}]({authorlink})|[{license}]({channel}/{plugin}/LICENSE)|{description}|\n'.format(name = data['name'], channel = channel,
 		            url = url, plugin = plugin, author = data['author'], authorlink = authorlink,
